91d0fdfac4eb5684b134a14e9e8189dbb490ac7d
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2024-11-28 16:44:32 -0300

    Initial Commit

---
+++ SsfAnimation/SsfAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SsfAnimationApp.swift
+//  SsfAnimation
+//
+//  Created by Marcelo Sampaio on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SsfAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
